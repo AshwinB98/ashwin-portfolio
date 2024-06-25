4ca774e8fdbd77e7327705bb15a32771656c24f2
Ashwin Baskaran <ashwin.softdev@gmail.com>
2024-06-25 16:56:30 +0530

    completed header components for mobile view.

---
+++ src/components/Header.jsx
@@ -0,0 +1,35 @@
+import styled from "styled-components";
+
+function Header() {
+  return (
+    <HeaderContainer>
+      <HeaderName>
+        <span>&#123; A &#125;</span> PORTFOLIO
+      </HeaderName>
+      <HeaderDropdown>
+        <p>ABOUT</p>
+        <p>PROJECTS</p>
+      </HeaderDropdown>
+    </HeaderContainer>
+  );
+}
+const HeaderContainer = styled.header`
+  padding: 2rem;
+  display: flex;
+  justify-content: space-between;
+`;
+const HeaderDropdown = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 3rem;
+  @media only screen and (max-width: 45rem) {
+    display: none;
+  }
+`;
+const HeaderName = styled.h1`
+  font-family: "Teko", sans-serif;
+  font-weight: 700;
+  color: #ff6600;
+`;
+
+export default Header;
