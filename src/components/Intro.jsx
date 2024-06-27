3aeeeb65adac09ef1c7e7d9afe3265722e6e69ad
Ashwin Baskaran <ashwin.softdev@gmail.com>
2024-06-27 19:14:40 +0530

    added animation for intro section.

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,10 +1,20 @@
+import Lottie from "react-lottie";
 import styled from "styled-components";
+import animationData from "../assets/coderAnime.json";
 import { HeaderName } from "./Header";
 
 function Intro({ onClickAbout }) {
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
   return (
     <IntroContainer>
-      <div style={{ padding: "1.4rem" }}>
+      <IntroContentContainer>
         <IntroTypography>Hello There!</IntroTypography>
         <NameTypography>
           <NameIntro>I&apos;M</NameIntro> ASHWIN
@@ -12,7 +22,10 @@ function Intro({ onClickAbout }) {
         <ProfessionTypography>Software Developer</ProfessionTypography>
         <DescTypography>Expertise in Web Technologies</DescTypography>
         <ContactMeButton onClick={onClickAbout}>Learn More</ContactMeButton>
-      </div>
+      </IntroContentContainer>
+      <IntroAnimationContainer>
+        <Lottie id="lottie-icon" options={defaultOptions} />
+      </IntroAnimationContainer>
     </IntroContainer>
   );
 }
@@ -21,7 +34,23 @@ const IntroContainer = styled.div`
   display: flex;
   height: 100%;
   width: 100%;
-  justify-content: space-between;
+  justify-content: space-around;
+  @media only screen and (max-width: 45rem) {
+    flex-direction: column;
+  }
+`;
+
+const IntroContentContainer = styled.div`
+  padding: 1.4rem;
+  @media only screen and (max-width: 45rem) {
+    order: 2;
+  }
+`;
+
+const IntroAnimationContainer = styled.div`
+  @media only screen and (max-width: 45rem) {
+    padding-top: 1.8rem;
+  }
 `;
 const IntroTypography = styled.h1`
   font-size: 3.8rem;
